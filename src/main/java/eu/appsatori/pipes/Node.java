56d50edf37b5ba48fcc96dfffde8eb5b850f9dbd
Vladimír Oraný <vladimir@orany.cz>
2012-02-11 19:49:40 +0100

    docs

--- src/main/java/eu/appsatori/pipes/Node.java
+++ src/main/java/eu/appsatori/pipes/Node.java
@@ -16,9 +16,43 @@
 
 package eu.appsatori.pipes;
 
+import java.io.Serializable;
 
+/**
+ * Node is basic building block of pipes. 
+ * 
+ * Use then to perform task in certain queue. 
+ * 
+ * If you want to perform task in different queue than default one,
+ * use {@link Queue} annotation on the implementation class.
+ * 
+ * @author Vladimir Orany
+ *
+ * @param <P> either {@link ParallelPipe} if the node is supposed to run in parallel or {@link SerialPipe} otherwise
+ * @param <A> type of the argument passed to the node. Must be {@link Serializable}
+ * 
+ * @see Pipes#run(Class)
+ * @see Pipes#run(Class, Serializable)
+ * @see Pipes#fork(Class, Serializable)
+ * @see Pipes#sprint(Class, Serializable)
+ */
 public interface Node<P extends Pipe, A> {
 	
+	/**
+	 * The task executed by this node.
+	 * 
+	 * Use methods of {@link SerialPipe} and {@link ParallelPipe}
+	 * to control the flow of the pipe. 
+	 * The flow is influenced only if the methods are used in return statements such as
+	 * 
+	 * <code>return pipe.next(MyTask, params);</code>
+	 * 
+	 * To start another pipe as a side effect of the task use one of the {@link Pipes} methods.
+	 * 
+	 * @param pipe one of {@link ParallelPipe} or {@link SerialPipe} pipe controllers
+	 * @param arg parameeter of the task
+	 * @return {@link NodeResult} signaling following pipe flow
+	 */
 	NodeResult execute(P pipe, A arg);
 
 }
